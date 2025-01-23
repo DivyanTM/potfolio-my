6c81d22f080e2536c5fefd0b8620769928e692df
DivyanTM <divyanthiruvalarselvan@gmail.com>
2025-01-23 23:15:07 +0530

    included new sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,45 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        slideProgress: {
+          '0%': { width:'0%' },
+          '100%': { width:'inherit' },
+        },
+      },
+      animation: {
+        progressSlide: 'slideProgress 1s ease-in-out forwards',
+      },
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif'],
+        Tangerine:['Tangerine','sans-serif'],
+        Roboto:['Roboto','sans-serif']
+      },
+      fontSize:{
+        'xxs':'13px',
+        'xxxs':'9.5px'
       },
       backgroundImage: {
         'underline': "url('/src/unser.svg')",
       },
       spacing: {
-        '24': '6rem',    // Adding a new margin class m-24
-        '28': '7rem',    // Adding a new margin class m-28
-        '32': '8rem',    // Adding a new margin class m-32
+        '24': '6rem',    
+        '28': '7rem',    
+        '32': '8rem',    
         '34': '10rem',
         '36':'12rem'
       }, 
+      width:{
+        0.1:'1px'
+      },
+      borderRadius:{
+        'semi':'45%'
+      },
+      
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
 
